207affca96747e305b90b6b0ece2a9f269761d85
sivaar1 <sivaar1@umbc.edu>
2017-11-15 16:53:20 -0500

    Made dropdowns look nicer

--- resources/views/applicants/edit.blade.php
+++ resources/views/applicants/edit.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.app')
 
+@section('head')
+    <style>
+        .form-control-dropdown 
+        {
+            width: auto;
+        }
+        .form-control-input{
+            width: 50%;
+        }
+    </style>
+@endsection
+
 @section('content')
     <h1 align="center">Edit Applicant</h1>
     {!! Form::open(['action' => ['ApplicantsController@update', $applicant->id], 'method' => 'POST']) !!}
@@ -21,7 +33,7 @@
         </div>
         <div class="form-group">
             {{Form::label('job_title', 'Job:')}}
-            {{Form::select('job_title', $jobArray, $applicant->job_title)}}
+            {{Form::select('job_title', $jobArray, $applicant->job_title, ['class' => 'form-control form-control-dropdown'])}}
         </div>
         <div class="form-group">
             {{Form::label('availability', 'Availability:')}}
@@ -34,15 +46,15 @@
             'Schedule Phone Screen' => 'Schedule Phone Screen', 'Phone Screen Completed, Schedule Interview' => 'Phone Screen Completed, Schedule Interview',
             'Interview Complete' => 'Interview Complete', 'Wait and See' => 'Wait and See', 'Offer' => 'Offer', 'Not in Consideration' => 'Not in Consideration',
             'Hired - Offer Accepted' => 'Hired - Offer Accepted', 'Offer Rejected' => 'Offer Rejected'], 
-            $applicant->status)}}
+            $applicant->status, ['class' => 'form-control form-control-dropdown'])}}
         </div>
         <div class="form-group">
             {{Form::label('close', 'Conveniant to Columbia:')}}
-            {{Form::select('close', ['' => '', 'Yes' => 'Yes', 'No' => 'No'], $applicant->close)}}
+            {{Form::select('close', ['' => '', 'Yes' => 'Yes', 'No' => 'No'], $applicant->close, ['class' => 'form-control form-control-dropdown'])}}
         </div>
         <div class="form-group">
             {{Form::label('contractor', 'Willing to Contract?')}}
-            {{Form::select('contractor', ['' => '', 'Yes' => 'Yes', 'No' => 'No'], $applicant->contractor)}}
+            {{Form::select('contractor', ['' => '', 'Yes' => 'Yes', 'No' => 'No'], $applicant->contractor, ['class' => 'form-control form-control-dropdown'])}}
         </div>
         <div class="form-group">
             {{Form::label('salary', 'Salary:')}}
