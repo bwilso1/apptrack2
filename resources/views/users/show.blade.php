6b228939e081c8417aba1d356cff2e3a88d8accc
sivaar1 <sivaar1@umbc.edu>
2017-11-22 00:34:07 -0500

    Small change in user display

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -2,13 +2,12 @@
 
 @section('content')
     <h1 align="center">{{$user->name}}</h1> <br>
-    <a class="btn btn-default" href="/users">Back</a>
     <table class="table">
         <tr><h3><td><strong>Name:</strong></td> <td>{{$user->name}}</td></h3></tr>
         <tr><h3><td><strong>ID:</strong></td> <td>{{$user->id}}</td></h3></tr>
         <tr><h3><td><strong>Email:</strong></td> <td>{{$user->email}}</td></h3></tr>
         <tr><h3><td><strong>Role:</strong></td> <td>{{$user->role}}</td></h3></tr> <br>
     </table>
-    <a class="btn btn-primary" style="margin-right:10px;" href="#">Edit</a>
-    <a class="btn btn-primary" href="#">Change Password</a>
+    <a class="btn btn-primary" href="#">Edit</a>
+    <a class="btn btn-default" href="/users">Back</a> 
 @endsection
